aad1dd658139c66e3d3e4c11c28abdd0f83699ac
Abhay Venkatesh <abhay.venkatesh@gmail.com>
2017-06-27 17:09:44 -0700

    Working lines

---
+++ Chairbot Interface/DrawView.swift
@@ -0,0 +1,53 @@
+//
+//  DrawView.swift
+//  Chairbot Interface
+//
+//  Created by Abhay Venkatesh on 6/26/17.
+//  Copyright © 2017 Stanford CDR. All rights reserved.
+//
+
+import UIKit
+
+class DrawView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    var lines: [Line] = []
+    var lastPoint: CGPoint!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lastPoint = touches.first?.location(in: self)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let newPoint = touches.first?.location(in: self)
+        lines.append(Line(start: lastPoint, end: newPoint!))
+        lastPoint = newPoint
+        
+        self.setNeedsDisplay()
+    }
+    
+    override func draw(_ rect: CGRect) {
+        let context = UIGraphicsGetCurrentContext()
+        context!.beginPath()
+        for line in lines {
+            context?.move(to: CGPoint(x: line.start.x, y: line.start.y))
+            context?.addLine(to: CGPoint(x: line.end.x, y: line.end.y))
+        }
+        context!.setStrokeColor(red: 0, green: 0, blue: 0, alpha: 1)
+        context!.setLineWidth(5)
+        context!.strokePath()
+    }
+    
+
+}
---
+++ Chairbot Interface/Line.swift
@@ -0,0 +1,19 @@
+//
+//  Line.swift
+//  Chairbot Interface
+//
+//  Created by Abhay Venkatesh on 6/26/17.
+//  Copyright © 2017 Stanford CDR. All rights reserved.
+//
+
+import UIKit
+
+class Line {
+    var start: CGPoint
+    var end: CGPoint
+    
+    init(start _start: CGPoint, end _end: CGPoint) {
+        start = _start
+        end = _end
+    }
+}
